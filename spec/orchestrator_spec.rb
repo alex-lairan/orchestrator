d0a924ccb8c46b85d29c741395ea22f9e6ae2f9b
alex-lairan <alexandrelairan+github@gmail.com>
2018-10-01 12:12:32 +0000

    First commit

---
+++ spec/orchestrator_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Orchestrator do
+  it "has a version number" do
+    expect(Orchestrator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
